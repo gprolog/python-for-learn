d00a6d18fefc3e17bfda30e06ac34a9ee9ead5f2
pangxie1987 <lpb.waln@outlook.com>
2017-09-06 19:37:04 +0800

    user selenium webdriver

--- GitHub/sinanews/zhihu.py
+++ GitHub/sinanews/zhihu.py
@@ -1,22 +1,22 @@
-'''
-.//*[@id='root']/div/main/div/div/div[1]/div[2]/div/div[3]/div/div[2]/h2/div/a
-.//*[@id='root']/div/main/div/div/div[1]/div[2]/div/div[2]/div/div[3]/h2/div/a
-.//*[@id='root']/div/main/div/div/div[1]/div[2]/div/div[1]/div/div[3]/h2/div/a
-.//*[@id='root']/div/main/div/div/div[1]/div[2]/div/div[6]/div/div[2]/h2/div/a
-'''
 import requests
 from lxml import etree
 import urllib.request
-
-
-url='https://www.zhihu.com/question/63798472'
-html=urllib.request.urlopen(url).read()
-#html=urllib.request.urlopen(url).read().decode("utf-8")
+import selenium.webdriver.support.ui as ui
+from selenium import webdriver
+#从知乎抓取热门回答
+url='https://www.zhihu.com/question/64615364'
+'''
+#1、使用requests库取不到文本内容，换成urllib.request.urlopen(url).read()这种方式
+html=urllib.request.urlopen(url).read().decode('utf-8')
 #html=requests.get(url).content
-print(html)
-
 page=etree.HTML(html)
-print(page)
-hottitle=page.xpath("//span[@class='RichText CopyrightRichText-richText']/text()")
+#print(page)
+'''
+#2、使用webdriver方法
+driver=webdriver.PhantomJS()
+driver.get(url)
 
-print(len(hottitle))
+#使用class类的方式抓取这一类下面的文本,使用element方法,注：使用element只匹配到的第一个
+hottitle=driver.find_elements_by_xpath("//*[@class='RichText CopyrightRichText-richText']")
+for alltitle in hottitle:
+    print(alltitle.text)
